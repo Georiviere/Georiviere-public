9e6953457ff15262de818e0882bee78871089cd2
Florian Sommariva <florian.sommariva@makina-corpus.com>
2023-06-01 13:17:45 +0200

    Pimp the loading for the map

--- src/components/map-Wrapper.tsx
+++ src/components/map-Wrapper.tsx
@@ -3,20 +3,36 @@
 import { HTMLAttributes, useMemo } from 'react';
 import dynamic from 'next/dynamic';
 import { usePathname } from 'next/navigation';
+import { useTranslations } from 'next-intl';
 
 import { cn } from '@/lib/utils';
+
+import { Icons, propsForSVGPresentation } from './icons';
 
 type Props = HTMLAttributes<HTMLElement>;
 
 export default function MapWrapper({ className, ...props }: Props) {
   const pathName = usePathname();
+  const t = useTranslations('map');
   const Map = useMemo(
     () =>
       dynamic(() => import('@/components/map'), {
-        loading: () => <span>Loading</span>,
+        loading: () => (
+          <div className="grid h-full place-content-center bg-secondary">
+            <div className="flex flex-col items-center gap-2">
+              <Icons.loading
+                className="animate-spin text-primary"
+                height={48}
+                width={48}
+                {...propsForSVGPresentation}
+              />
+              <span>{t('loading')}</span>
+            </div>
+          </div>
+        ),
         ssr: false,
       }),
-    [],
+    [t],
   );
 
   return (
