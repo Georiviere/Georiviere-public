b1fa0829005bb699e8459ded3e6b5eda1326e762
Florian Sommariva <florian.sommariva@makina-corpus.com>
2023-07-28 18:45:45 +0200

    Use GeometryList component instead of GeoJSON

--- src/components/map/geojson.tsx
+++ src/components/map/geojson.tsx
@@ -1,15 +1,8 @@
 import { useMemo } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { Layer } from '@/api/settings';
-import { useMapContext } from '@/context/map';
-import { Feature } from 'geojson';
-import L, { Icon, LatLng, Layer as LeafletLayer } from 'leaflet';
-import { renderToStaticMarkup } from 'react-dom/server';
-import { GeoJSON } from 'react-leaflet';
 
-import { Icons } from '@/components/icons';
-import { DefaultMarker } from '@/components/map/default-marker';
-import Popup from '@/components/map/popup';
+import { GeometryList } from '@/components/map/geometry-list';
 
 type Props = {
   layer: Layer;
@@ -17,52 +10,6 @@ type Props = {
 
 export default function GeoJson({ layer }: Props) {
   const searchParams = useSearchParams();
-  const { layers } = useMapContext();
-
-  const onEachFeaturehandler = (
-    feature: Feature,
-    layer: LeafletLayer,
-    type: string,
-  ) => {
-    const { properties } = feature;
-
-    if (properties === null || !properties.name) {
-      return;
-    }
-
-    if (properties.name) {
-      layer.bindTooltip(properties.name);
-    }
-
-    if (layers === null || layers.length === 0) {
-      return;
-    }
-
-    if ('url' in (layers.find(item => item.type === type) || {})) {
-      layer.bindPopup(
-        renderToStaticMarkup(
-          <Popup
-            name={properties.name}
-            description={properties.description}
-            attachments={properties.attachments}
-            type={type}
-            id={properties.id}
-            params={searchParams}
-          />,
-        ),
-        { offset: [4, -14] },
-      );
-    }
-  };
-
-  const pointToLayerHandler = (feature: Feature, latlng: LatLng) => {
-    const icon =
-      feature.properties?.type?.pictogram ??
-      renderToStaticMarkup(<Icons.info fill="white" />);
-    return L.marker(latlng, {
-      icon: DefaultMarker(icon, 1) as Icon,
-    });
-  };
 
   const data = useMemo(() => {
     const searchText = searchParams.get('text');
@@ -85,11 +32,10 @@ export default function GeoJson({ layer }: Props) {
   }
 
   return (
-    <GeoJSON
-      data={data}
-      style={layer.options.style}
-      onEachFeature={(...args) => onEachFeaturehandler(...args, layer.type)}
-      pointToLayer={pointToLayerHandler}
+    <GeometryList
+      contents={data.features}
+      layer={layer}
+      options={layer.options}
     />
   );
 }
