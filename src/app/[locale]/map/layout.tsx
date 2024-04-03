bba99558dc4b7a7bfe6fa73022dc53f4b41cd5d6
Maxime Bouveron <maxime.bouveron@gmail.com>
2024-04-03 16:09:59 +0200

    Add two-panels mode for tablets

--- src/app/[locale]/map/layout.tsx
+++ src/app/[locale]/map/layout.tsx
@@ -32,12 +32,12 @@ export default async function MapLayout({ children }: Props) {
     <MapContextProvider defaultSettings={settings}>
       <main
         role="main"
-        className="grid h-full w-screen grid-cols-[100dvw_auto_100dvw] grid-rows-1 justify-stretch overflow-x-hidden scroll-smooth md:grid-cols-[300px_auto_1fr]"
+        className="grid h-full w-screen grid-cols-[100dvw_auto_100dvw] grid-rows-1 justify-stretch overflow-x-hidden scroll-smooth md:grid-cols-[50dvw_auto_50dvw] lg:grid-cols-[300px_auto_1fr]"
       >
         <MapSidebar id="search" className="z-[1001]" />
         <section
           id="content"
-          className="h-full w-[100dvw] empty:w-0 md:w-[350px] lg:w-[450px]"
+          className="h-full w-[100dvw] empty:w-0 md:w-[50dvw] lg:w-[350px] xl:w-[450px]"
         >
           {children}
         </section>
