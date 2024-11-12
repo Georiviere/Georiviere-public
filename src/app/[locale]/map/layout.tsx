d576656eef9407459576a25c6ffcaf739ae9cc53
Florian Sommariva <1926041+dtrucs@users.noreply.github.com>
2024-11-12 17:18:04 +0100

    Call getObservations in server side

--- src/app/[locale]/map/layout.tsx
+++ src/app/[locale]/map/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import { getObservations } from '@/api/customObservations';
 import { MapContextProvider } from '@/context/map';
 import { getTranslations } from 'next-intl/server';
 
@@ -19,13 +20,18 @@ export const generateMetadata = async () => {
 };
 
 export default async function MapLayout({ children }: Props) {
+  const observations = await getObservations();
   return (
     <MapContextProvider>
       <main
         role="main"
         className="grid h-full w-screen grid-cols-[100dvw_auto_100dvw] grid-rows-1 justify-stretch overflow-x-hidden scroll-smooth md:grid-cols-[50dvw_auto_50dvw] lg:grid-cols-[300px_auto_1fr]"
       >
-        <MapSidebar id="search" className="z-[1001]" />
+        <MapSidebar
+          id="search"
+          className="z-[1001]"
+          observations={observations}
+        />
         <section
           id="content"
           className="h-full w-[100dvw] empty:w-0 md:w-[50dvw] lg:w-[350px] xl:w-[450px]"
